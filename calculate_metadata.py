8fd6b0e8919cc184976255f0c45cce640d0d74ff
Marshall Thompson <mdt_@gwu.edu>
2021-10-18 20:24:01 -0400

    add script to calculate metadata

---
+++ calculate_metadata.py
@@ -0,0 +1,26 @@
+from tqdm import tqdm
+from util import open_pckl_file
+from numba import jit
+import logging 
+
+def get_data(h_ids):
+    min_num = 0
+    max_num = 0
+    total = 0
+
+    for h in tqdm(h_ids):
+        num_h = len(r2d2.get_room_ids(h))
+        min_num = min(min_num, num_h)
+        max_num = max(max_num, num_h)
+        total += num_h
+    return min_num, max_num, total
+
+if __name__ =='__main__':
+    logging.basicConfig(filename='./metadata.txt')
+
+    r2d2 = open_pckl_file('./r2d2_features.pckl')
+
+    h_ids = r2d2.get_hotel_ids()
+    min_num, max_num, total = get_data(h_ids)
+    message = "total={}\nmin={}\nmax={}\navg=".format(total, min_num, max_num, total/len(h_ids))
+    logging.info(message)
